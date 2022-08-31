d32f8f73b08946cfb1b4c69ec19d075a3983e724
Nathan Delman <delman.nathan832@gmail.com>
2022-08-30 19:30:02 -0500

    popup

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+
+
+setTimeout(() => {
+  reLocate();
+  console.log('here')
+}, "1000")
+
+const reLocate=()=>{
+    window.location.replace('https://nathan-delman.netlify.app')
+}
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    
-    if (req.headers['user-agent'].includes('curl')){
-      res.sendFile(path.join(__dirname, 'public', 'hype.html'));
-    }else{
-      res.sendFile(path.join(__dirname, 'public', 'honest.html'))
-    }
-  });
+app.get("/", (req, res) => {
+  if (req.headers["user-agent"].includes("curl")) {
+    res.sendFile(path.join(__dirname, "public", "hype.html"));
+  } else {
+    res.sendFile(path.join(__dirname, "public", "honest.html"));
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
